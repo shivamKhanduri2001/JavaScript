82c82140ff4e7b12c748a1af6877bfcde56b977a
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-05-25 12:35:25 +0530

    Implement prime number checker with user input

---
+++ practrise/prime.js
@@ -0,0 +1,46 @@
+const { checkPrime } = require('crypto');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+function isPrime(num){
+    if(num <= 1) return false;
+    for (let i = 2; i < num; i++){
+        if (num % i === 0) return false;
+        return true;
+    };
+};
+
+function askNumber(){
+    rl.question('Please enter a number: ', function(answer){
+        let num = parseInt(answer);
+        if (isNaN(num)){
+            console.log('Invalid Input')
+            askNumber()
+        } else if (isPrime(num)) {
+            console.log('It is a prime number.');
+            askContinue();
+        } else {
+            console.log(`${num} is not a prime number`);
+            askContinue()
+        }
+         
+    });
+}
+function askContinue(){
+    rl.question('Do you want to continue? (y/n): ', function(answer){
+        if( answer === 'y'){
+            process.stdout.write('\x1Bc');
+            askNumber();
+        } else if (answer === 'n'){
+            console.log('GoodBye!');
+            rl.close();
+        } else {
+            askContinue();
+        }
+    });
+};
+
+askNumber();
