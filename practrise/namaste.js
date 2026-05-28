77e369191355fc047371dc7a0b71f1d4b027fddd
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-05-28 07:09:35 +0530

    Create namaste.js

---
+++ practrise/namaste.js
@@ -0,0 +1,8 @@
+<p id = "message">Hi</p>
+<button onclick = "greeting()">Click me</button>
+
+<script> 
+    function greeting(){
+        document.getElementById("message").innerHTML = "Good Morning"
+    }
+</script>
