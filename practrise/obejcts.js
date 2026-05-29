4c64767e0ef09845734d0b65361f8efe5c1e24f7
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-05-29 08:07:27 +0530

    Add user and order objects with console logs

---
+++ practrise/obejcts.js
@@ -0,0 +1,28 @@
+//make a laptop object
+const laptop = {
+  brand: "Hp",
+  price: "40k",
+  color: "black"
+};
+
+console.log(laptop.brand);
+
+// make data for insta
+const user = {
+  username: "Shivam",
+  followers: 400,
+  verified: true
+};
+
+console.log(user.verified);
+
+//Change the address and full print
+const order = {
+  item: "Burger",
+  price: 250,
+  address: "BurgerKing"
+};
+
+order.address = "Subway";
+
+console.log(order);
