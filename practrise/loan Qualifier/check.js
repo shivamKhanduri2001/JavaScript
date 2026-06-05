6eef0f9757a036423d03360bf037cad61f866934
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-05 12:36:40 +0530

    Add loan qualification logic in check.js

---
+++ practrise/loan Qualifier/check.js
@@ -0,0 +1,16 @@
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if (annualIncome >= minIncomeForDuplex && creditScore >= minCreditScoreForDuplex) {
+    return "You qualify for a duplex, condo, and car loan.";
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan.";
+  }
+}
