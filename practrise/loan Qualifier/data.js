86f59ca0bfc1af1c4550dc973f5c91c5aede4046
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-05 11:44:33 +0530

    Create data.js

---
+++ practrise/loan Qualifier/data.js
@@ -0,0 +1,8 @@
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
