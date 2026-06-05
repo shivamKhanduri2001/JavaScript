77ce83ddb2716fa9dab462febc0b87521ce9ff2b
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-05 10:53:45 +0530

    Create substract.js

---
+++ practrise/calculator/substract.js
@@ -0,0 +1,7 @@
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));
