a4230abd4b85326f4cf4af80ca91abdb7d657924
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-05 11:02:22 +0530

    Add division by zero check in calculateQuotient function

---
+++ practrise/calculator/check zero.js
@@ -0,0 +1,6 @@
+function calculateQuotient(num1, num2) {
+  return num2 === 0 ? "Error: Division by zero" : num1 / num2;
+}
+
+console.log(calculateQuotient(7, 11));
+console.log(calculateQuotient(3, 0));
