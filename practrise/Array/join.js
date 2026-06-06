f7e24cf3fcd77911b010b3943c0cd79ace1bdde6
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-06 10:38:02 +0530

    Create join.js

---
+++ practrise/Array/join.js
@@ -0,0 +1,7 @@
+let fruit = ["m","a", "n" ,"g", "o"];
+let normal = fruit.join("");
+console.log(normal);
+
+let reversedArray = ["o", "l", "l", "e", "h"];
+let reversedString = reversedArray.join("");
+console.log(reversedString);
