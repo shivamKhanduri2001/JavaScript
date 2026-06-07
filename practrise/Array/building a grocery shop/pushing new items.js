c8534595b5f8f1cb63271b0510a38c5ea96c6546
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-07 08:14:41 +0530

    shoppingList.js

---
+++ practrise/Array/building a grocery shop/pushing new items.js
@@ -0,0 +1,11 @@
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
