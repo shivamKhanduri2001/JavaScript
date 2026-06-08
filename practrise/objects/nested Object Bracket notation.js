7ce0f07bee95cf5a3e91f13e700bcf01478aaa4f
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-08 19:00:46 +0530

    Create nested Object Bracket notation.js

---
+++ practrise/objects/nested Object Bracket notation.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Shivam",
+    contact: {
+        email: "khandurishivam4@gmail.com",
+        number: {
+            home: 1234567,
+            office: 3214
+        }
+    }
+};
+console.log(person["contact"]["number"]["office"]);
