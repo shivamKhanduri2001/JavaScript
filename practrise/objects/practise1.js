9a057cf8021a386a594a870b425b6f91adfae86c
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-08 19:08:57 +0530

    Add person object with contact phone numbers

---
+++ practrise/objects/practise1.js
@@ -0,0 +1,10 @@
+let person = {
+    contact: {
+        phone: {
+            home: "123-456-7890",
+            work: "098-765-4321"
+        }
+    }
+};
+
+console.log(person.contact.phone.work);
