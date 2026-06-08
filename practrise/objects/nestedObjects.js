550e728e93c143afe2f6c5f48bd6c27559ce2b19
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-08 18:55:39 +0530

    Add nested object structure for person details

---
+++ practrise/objects/nestedObjects.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "Shivam",
+  contact: {
+    email: "khandurishivam4@gmail.com",
+    phone: {
+      home: 123456789,
+      office: 3214
+    }
+  }
+};
+console.log(person.contact.phone.office);
