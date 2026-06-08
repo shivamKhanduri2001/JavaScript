947da4537719f3279f76eb6dac0a184efccd04e9
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-08 19:07:17 +0530

    Add nested object for person with addresses

---
+++ practrise/objects/nested object for array.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Shivam",
+    addresses: [
+        { type: "home", city: "london" },
+        { type: "office", city: "new york"}
+    ]
+};
+
+console.log(person.addresses[1].city);
